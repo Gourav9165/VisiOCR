6be553530ee58544b4a7c66eea42ef58e08079bc
Gourav Sen <sen9165812790@gmail.com>
2024-07-15 16:42:29 +0530

    Delete visiOCR_app directory

--- visiOCR_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VisiocrAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'visiOCR_app'
--- visiOCR_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('extract', views.extractText, name="extract_text"),
-    path('download', views.downloadPDF, name="download_pdf"),
-]
